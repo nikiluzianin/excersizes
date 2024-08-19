a25a89a70b99f520c1806e5f40070d6266e40a85
Nikita Luzianin <s2400796@edu.bc.fi>
2024-08-19 14:29:09 +0300

    week 2 monday

---
+++ 02_week/02_exercise/numbers/numbers_02.js
@@ -0,0 +1,10 @@
+// Define a function to convert a temperature from Fahrenheit to Celsius.
+// This function uses the formula (Fahrenheit - 32) * 5/9
+// The resulting temperature is rounded to one decimal place for precision.
+
+function fahrenheitToCelsius(Fahrenheit) {
+    return parseFloat(((Fahrenheit - 32) * 5/9).toFixed(1))
+}
+
+// Sample usage - do not modify
+console.log(fahrenheitToCelsius(21)); // "-6,1"
---
+++ 02_week/02_exercise/strings/strings_10.js
@@ -0,0 +1,12 @@
+/**
+Refine the capitalize function to ensure it capitalizes the first letter of the name parameter provided to it.
+ */
+
+function capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1, word.length).toLowerCase();
+}
+
+// Sample usage - do not modify
+console.log(capitalize('sam')); // "Sam"
+console.log(capitalize('ALEX')); // "Alex"
+console.log(capitalize('chARLie')); // "Charlie"
