2d13db6654a08e90418d7656cb62be4ff5179f91
Nikita Luzianin <s2400796@edu.bc.fi>
2024-09-09 12:23:03 +0300

    add looping through options and new html elements]

--- 04_week/04_exercise/pancake_maker/script.js
+++ 04_week/04_exercise/pancake_maker/script.js
@@ -13,17 +13,35 @@ const syrupToppingSelector = document.querySelector('#syrup');
 const creamExtraSelector = document.querySelector('#whippedCream'); //extras
 const iceExtraSelector = document.querySelector('#iceCream');
 
-const priceForTopping = 1;
+const customerName = document.querySelector('#customerName');
+
+const deliveryOption = document.querySelector('#delvieryOptions');
+
+//const
+
+//const priceForTopping = 1;
+
+let allAdditionalSelectors = [nutsToppingSelector, bananasToppingSelector, syrupToppingSelector, creamExtraSelector, iceExtraSelector];
 
 updatePrice = () => {
     let newPrice = parseInt(typeSelector.value, 10); // getting the base price
 
+    for (let selector of allAdditionalSelectors) {
+        (selector.checked) && (newPrice += parseInt(selector.value, 10));
+    } // loop through all the selectors
+
+    /*
     (nutsToppingSelector.checked) && (newPrice = newPrice + priceForTopping); // adding the topings
     (bananasToppingSelector.checked) && (newPrice = newPrice + priceForTopping);
     (syrupToppingSelector.checked) && (newPrice = newPrice + priceForTopping);
 
     (creamExtraSelector.checked) && (newPrice = newPrice + parseInt(creamExtraSelector.value, 10));
     (iceExtraSelector.checked) && (newPrice = newPrice + parseInt(iceExtraSelector.value, 10));
+    */ // lame solution for the selectors
+
+
+
+
 
     for (let totalPriceField of totalPriceFields) totalPriceField.textContent = '$' + newPrice;
 }
