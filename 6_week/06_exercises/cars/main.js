a4977a0d782ddbe13182bfdddf290223bba014e5
Nikita Luzianin <s2400796@edu.bc.fi>
2024-09-25 12:35:25 +0300

    update cars fixed everything (css missing)

--- 6_week/06_exercises/cars/main.js
+++ 6_week/06_exercises/cars/main.js
@@ -16,8 +16,11 @@ const carDataErrors = document.querySelector("#car-data-errors");
 
 
 const platesFormatRegex = /^[a-zA-Z]{3}\d{3}$/;
-const currentYear = 2024;
-const discountPercentage = 0.85;
+const EARLIEST_YEAR = 1880;
+const CURRENT_YEAR = 2024;
+const DISCOUNT_PERCENTAGE = 0.85;
+
+
 const cars = [];
 
 
@@ -33,173 +36,136 @@ class Car {
     }
 }
 
+// input checks
+const checkSearchInput = (searchInput) => {
+    const allErrors = [];
+    if (searchInput.trim() == 0)
+        allErrors.push("License number filled with spaces");
+    else if (!platesFormatRegex.test(searchInput))
+        allErrors.push("License number is not correct. Should be in format ABC123");
 
-const checkSearchInput = () => {
-    let allCorrect = true;
     try {
-        if (searchText.value == "") {
-            throw new Error("License number empty");
-        } else if (!platesFormatRegex.test(searchText.value)) {
-            throw new Error("License number is not correct. Should be in format ABC123")
-        }
-
+        if (allErrors.length != 0)
+            throw new Error("Input errors:\n" + allErrors.join("\n") + '\n');
     } catch (error) {
-        searchResult.textContent = error.message;
-        allCorrect = false;
+        printSearchResults(error.message);
     }
-    return allCorrect;
+
+    return (allErrors.length == 0);
 }
-
-const searchCar = (searchingLicense) => {
-    return cars.find((car) => car.license == searchingLicense);
-}
-
-const searchFromSearchBar = () => {
-    if (checkSearchInput()) {
-        const foundCar = searchCar(searchText.value);
-        if (foundCar) {
-            const discountPriceInfo = (currentYear - foundCar.year > 10) ? (", discounted price: " + foundCar.price * discountPercentage) : "";
-            const carInfo = "make: " + foundCar.maker + ", model: " + foundCar.model + ", owner: " + foundCar.owner + ", price: " + foundCar.price + discountPriceInfo;
-            searchResult.textContent = "Car found, info: \n" + carInfo;
-        } else searchResult.textContent = "No results found"
-    }
-}
-
-const addCarToTable = (car) => {
-
-    const tableRow = document.createElement('tr');
-
-    const discountPrice = (currentYear - car.year > 10) ? car.price * discountPercentage : "";
-
-    for (let [key, value] of Object.entries(car)) {
-        const tableCell = document.createElement('td');
-        tableCell.textContent = value;
-        tableRow.appendChild(tableCell);
-        if (key == "price") {
-            const tableCell = document.createElement('td');
-            tableCell.textContent = (discountPrice == "") ? "" : discountPrice;
-            tableRow.appendChild(tableCell);
-        }
-    }
-    tableBody.appendChild(tableRow);
-}
-
-const cleanErrors = () => carDataErrors.textContent = "";
-
-const printErrors = (allErrors) => carDataErrors.textContent += allErrors;
 
 const checkInputs2 = () => {
 
-    const inputErrors = [];
     const validationErrors = [];
-    cleanErrors();
+
+    resetErrors();
 
     if (licenseText.value.trim() == 0)
-        inputErrors.push("License number empty");
+        validationErrors.push("License number filled with spaces");
+    else if (!platesFormatRegex.test(licenseText.value))
+        validationErrors.push("License number is not correct");
+    else if (searchCar(licenseText.value))
+        validationErrors.push("Car with such license number already exists");
+
     if (makerText.value.trim().length == 0)
-        inputErrors.push("Car maker empty");
+        validationErrors.push("Car maker filled with spaces");
+
     if (modelText.value.trim().length == 0)
-        inputErrors.push("Car model empty");
+        validationErrors.push("Car model filled with spaces");
+
     if (yearText.value.trim().length == 0)
-        inputErrors.push("Car year empty");
+        validationErrors.push("Car year filled with spaces");
+    else if ((yearText.value < EARLIEST_YEAR) || (yearText.value > CURRENT_YEAR))
+        validationErrors.push("Car year should be between " + EARLIEST_YEAR + " and " + CURRENT_YEAR);
+
     if (ownerText.value.trim().length == 0)
-        inputErrors.push("Car owner empty");
+        validationErrors.push("Car owner filled with spaces");
+
     if (priceText.value.trim().length == 0)
-        inputErrors.push("Car price empty");
-    if (colorText.value.trim().length == 0)
-        inputErrors.push("Car color empty");
-
-    try {
-        if (inputErrors.length != 0)
-            throw new Error("Input errors:\n" + inputErrors.join("\n") + '\n');
-    } catch (error) {
-        printErrors(error.message);
-    }
-
-    if (!platesFormatRegex.test(licenseText.value))
-        validationErrors.push("License number is not correct")
-    if (searchCar(licenseText.value))
-        validationErrors.push("Car with such license number already exists")
-    if ((yearText.value < 1900) || (yearText.value > currentYear))
-        validationErrors.push("Car year should be between 1900 and " + currentYear);
-    if (priceText.value < 0)
+        validationErrors.push("Car price filled with spaces");
+    else if (priceText.value < 0)
         validationErrors.push("Car price should be positive");
+
+    if (colorText.value.trim().length == 0)
+        validationErrors.push("Car color filled with spaces");
 
     try {
         if (validationErrors.length != 0)
             throw new Error("Input errors:\n" + validationErrors.join("\n") + '\n');
     } catch (error) {
-        printErrors(error.message);
+        printCarErrors(error.message);
     }
 
-    return ((inputErrors.length == 0) && (validationErrors.length == 0));
-
-    /*
-    let allErrors = []; // TODO
-    
- 
-    if (actuallyCheck) {
-        try {
-            if (licenseText.value == "") {
-                throw new Error("License number empty");
-            } else if (!platesFormatRegex.test(licenseText.value)) {
-                throw new Error("License number is not correct")
-            } else if (searchCar(licenseText.value)) {
-                throw new Error("Car with such license number already exists")
-            }
-        } catch (error) {
-            allErrors = allErrors + error.message + "\n";
-        }
- 
-        try {
-            if (makerText.value == "")
-                throw new Error("Car maker empty");
-        } catch (error) {
-            allErrors = allErrors + error.message + "\n";
-        }
- 
-        try {
-            if (modelText.value == "") throw new Error("Car model empty");
-        } catch (error) {
-            allErrors = allErrors + error.message + "\n";
-        }
- 
-        try {
-            if (yearText.value == "") {
-                throw new Error("Car year empty");
-            } else if ((yearText.value < 1900) || (yearText.value > currentYear)) {
-                throw new Error("Car year should be between 1900 and " + currentYear);
-            }
-        } catch (error) {
-            allErrors = allErrors + error.message + "\n";
-        }
- 
-        try {
-            if (ownerText.value == "") throw new Error("Car owner empty");
-        } catch (error) {
-            allErrors = allErrors + error.message + "\n";
-        }
- 
-        try {
-            if (priceText.value == "") {
-                throw new Error("Car price empty");
-            } else if (priceText.value < 0) {
-                throw new Error("Car preice should be positive");
-            }
-        } catch (error) {
-            allErrors = allErrors + error.message + "\n";
-        }
- 
-        try {
-            if (colorText.value == "") throw new Error("Car color empty");
-        } catch (error) {
-            allErrors = allErrors + error.message + "\n";
-        }
- 
-        carDataErrors.textContent = allErrors;
-    }
-    return (allErrors == "");*/
+    return ((validationErrors.length == 0));
 }
+
+/*
+const checkInputs2 = () => {
+let allErrors = []; // TODO
+ 
+ 
+if (actuallyCheck) {
+    try {
+        if (licenseText.value == "") {
+            throw new Error("License number empty");
+        } else if (!platesFormatRegex.test(licenseText.value)) {
+            throw new Error("License number is not correct")
+        } else if (searchCar(licenseText.value)) {
+            throw new Error("Car with such license number already exists")
+        }
+    } catch (error) {
+        allErrors = allErrors + error.message + "\n";
+    }
+ 
+    try {
+        if (makerText.value == "")
+            throw new Error("Car maker empty");
+    } catch (error) {
+        allErrors = allErrors + error.message + "\n";
+    }
+ 
+    try {
+        if (modelText.value == "") throw new Error("Car model empty");
+    } catch (error) {
+        allErrors = allErrors + error.message + "\n";
+    }
+ 
+    try {
+        if (yearText.value == "") {
+            throw new Error("Car year empty");
+        } else if ((yearText.value < 1900) || (yearText.value > currentYear)) {
+            throw new Error("Car year should be between 1900 and " + currentYear);
+        }
+    } catch (error) {
+        allErrors = allErrors + error.message + "\n";
+    }
+ 
+    try {
+        if (ownerText.value == "") throw new Error("Car owner empty");
+    } catch (error) {
+        allErrors = allErrors + error.message + "\n";
+    }
+ 
+    try {
+        if (priceText.value == "") {
+            throw new Error("Car price empty");
+        } else if (priceText.value < 0) {
+            throw new Error("Car preice should be positive");
+        }
+    } catch (error) {
+        allErrors = allErrors + error.message + "\n";
+    }
+ 
+    try {
+        if (colorText.value == "") throw new Error("Car color empty");
+    } catch (error) {
+        allErrors = allErrors + error.message + "\n";
+    }
+ 
+    carDataErrors.textContent = allErrors;
+}
+return (allErrors == "");
+}*/
 
 const checkInputs1 = (actuallyCheck) => {
     if (actuallyCheck)
@@ -207,21 +173,59 @@ const checkInputs1 = (actuallyCheck) => {
     return true;
 }
 
+// actual function
+const addCar = (newCar) => {
+    cars.push(newCar);
+    addCarToTable(newCar);
+}
+
+
+// getting input from fields
+const searchFromSearchBar = (searchInput) => {
+    resetSearchResult();
+    if (checkSearchInput(searchInput)) {
+        const foundCar = searchCar(searchInput);
+        if (foundCar) {
+            const discountPriceInfo = (CURRENT_YEAR - foundCar.year > 10) ? (", discounted price: " + foundCar.price * DISCOUNT_PERCENTAGE) : "";
+            const carInfo = "make: " + foundCar.maker + ", model: " + foundCar.model + ", owner: " + foundCar.owner + ", price: " + foundCar.price + discountPriceInfo;
+            printSearchResults("Car found, info: \n" + carInfo);
+        } else printSearchResults("No results found");
+    }
+}
 
 const addCarFromFields = () => {
     if (checkInputs1(true)) {
-
         let newCar = new Car(licenseText.value, makerText.value, modelText.value, yearText.value, ownerText.value, priceText.value, colorText.value);
         addCar(newCar);
         resetFields();
     }
 }
 
-const addCar = (newCar) => {
-    cars.push(newCar);
-    addCarToTable(newCar);
+const searchCar = (searchingLicense) => cars.find((car) => car.license == searchingLicense);
+
+// modifying the table and error space
+const addCarToTable = (car) => {
+
+    let tableRow = tableBody.insertRow(-1);
+
+    const discountPrice = (CURRENT_YEAR - car.year > 10) ? car.price * DISCOUNT_PERCENTAGE : "";
+
+    for (let [key, value] of Object.entries(car)) {
+        let tableCell = tableRow.insertCell(-1);
+        tableCell.textContent = value;
+        if (key == "price") {
+            let tableCell = tableRow.insertCell(-1);
+            tableCell.textContent = (discountPrice == "") ? "" : discountPrice;
+        }
+    }
 }
 
+const printSearchResults = (results) => searchResult.textContent += results;
+
+const printCarErrors = (allErrors) => carDataErrors.textContent += allErrors;
+
+
+// cleaning the text fields
 const resetFields = () => {
     licenseText.value = "";
     makerText.value = "";
@@ -235,15 +239,20 @@ const resetFields = () => {
     searchResult.textContent = "";
 }
 
+const resetSearchResult = () => searchResult.textContent = "";
+
 const resetSearch = () => searchText.value = "";
 
+const resetErrors = () => carDataErrors.textContent = "";
+
+
 // listeners
-document.querySelector("#search").addEventListener("click", function (ev) {
+document.querySelector(".search-form").addEventListener("submit", function (ev) {
     ev.preventDefault();
     searchFromSearchBar(searchText.value);
 });
 
-document.querySelector("#addCar").addEventListener("click", function (ev) {
+document.querySelector(".form-cars").addEventListener("submit", function (ev) {
     ev.preventDefault();
     addCarFromFields();
 });
@@ -253,6 +262,7 @@ document.querySelector("#reset").addEventListener("click", function (ev) {
     resetFields();
 });
 
+// add some random cars for easier testing
 addCar(new Car("asd123", "VW", "Scirpccp", 2012, "Nikita", 10000, "Red"));
 addCar(new Car("asd124", "Audi", "A3", 2000, "Other person", 2000, "White"));
 
